a56a396d551142af5ef4b6b75afd97a241605887
santofemiano <santo.femiano001@studenti.uniparthenope.it>
2026-02-19 16:21:45 +0100

    Creazione API Rest

---
+++ src/main/java/com/yumaste/yumasteapi/YumasteApiApplication.java
@@ -0,0 +1,13 @@
+package com.yumaste.yumasteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumasteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YumasteApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yumaste/yumasteapi/YumasteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumaste.yumasteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumasteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
