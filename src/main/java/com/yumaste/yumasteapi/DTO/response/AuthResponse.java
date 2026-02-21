54f861a5c1926a569f878742cbea29441aef3d1f
santofemiano <santo.femiano001@studenti.uniparthenope.it>
2026-02-21 02:45:16 +0100

    Aggiunta delle classi DTO per la gestione delle richieste di autenticazione: AuthResponse, LoginRequest e RegisterRequest.

---
+++ src/main/java/com/yumaste/yumasteapi/DTO/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.yumaste.yumasteapi.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token; // Questo è il JWT che restituiremo al client
+}
