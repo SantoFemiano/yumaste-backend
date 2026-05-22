bf5c00e775a6292665297c8868e9bf1a86631379
Santo Femiano <74330010+SantoFemiano@users.noreply.github.com>
2026-05-22 16:57:05 +0200

    Revert "aggiunta la possibilità di fornire un suggerimento per la generazione…"

--- src/main/java/com/yumaste/yumasteapi/controllers/AdminController.java
+++ src/main/java/com/yumaste/yumasteapi/controllers/AdminController.java
@@ -46,10 +46,10 @@ public class AdminController {
 
     @PostMapping("/ai/genera-inserisci-ingredienti")
     public ResponseEntity<List<IngredienteResponseDTO>> generateAndInsertIngredientiAi(
-            @RequestParam(defaultValue = "1") int quantita, @RequestParam(defaultValue = "") String suggerimento) {
+            @RequestParam(defaultValue = "1") int quantita) {
 
         // 1. Chiedi all'IA di elaborare i DTO
-        List<IngredienteRequestDTO> requestsGenerati = aiDescriptionService.generaIngredientiNuovi(quantita,suggerimento);
+        List<IngredienteRequestDTO> requestsGenerati = aiDescriptionService.generaIngredientiNuovi(quantita);
 
         List<IngredienteResponseDTO> responseList = new ArrayList<>();
 
